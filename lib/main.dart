24e0d09fdd6bcc47b6f48e75b528461353e47de3
Xianbin <xianbin.yong13@sps.nus.edu.sg>
2020-07-17 19:43:12 +0800

    Fix typo in title

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pangya Calulator',
+      title: 'Pangya Calculator',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
